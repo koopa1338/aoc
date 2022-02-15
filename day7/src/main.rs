c90a220f57b0d87c5675f38a706eadcf82c8ff50
koopa1338 <sinner1991@gmail.com>
2022-02-15 01:47:09 +0100

    fixup! day7 part one

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -25,7 +25,7 @@ fn part_one(mut positions: Vec<usize>) -> usize {
     })
 }
 
-fn part_two() -> () {
+fn part_two(_positions: Vec<usize>) -> usize {
     unimplemented!();
 }
 
@@ -33,9 +33,10 @@ fn main() {
     let input = include_str!("../input.txt");
     let data = parse_input(input);
     println!("Part 1:");
-    println!("fuel needed: {}", part_one(data));
+    println!("fuel needed: {}", part_one(data.clone()));
 
     println!("Part 2:");
+    println!("fuel needed: {}", part_one(data));
 }
 
 #[cfg(test)]
@@ -46,19 +47,25 @@ mod tests {
     fn test_parse_data() {
         let input = "16,1,2,0,4,2,7,1,2,14";
         let data = parse_input(input);
-        let horiz = part_one(data);
 
-        assert_eq!(2, horiz);
-
+        assert_eq!(10, data.len());
     }
 
-//     #[test]
-//     fn test_part_one() {
-//         unimplemented!();
-//     }
+    #[test]
+    fn test_part_one() {
+        let input = "16,1,2,0,4,2,7,1,2,14";
+        let data = parse_input(input);
+        let fuel = part_one(data);
 
-//     #[test]
-//     fn test_part_two() {
-//         unimplemented!();
-//     }
+        assert_eq!(37, fuel);
+    }
+
+    /* #[test]
+    fn test_part_two() {
+        let input = "16,1,2,0,4,2,7,1,2,14";
+        let data = parse_input(input);
+        let fuel = part_two(data);
+
+        assert_eq!(168, fuel);
+    } */
 }
