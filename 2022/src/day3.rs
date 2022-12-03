5965561d97ae6f7cc8068b881f123c108a7dbd55
koopa1338 <sinner1991@gmail.com>
2022-12-03 13:34:36 +0100

    day3 solutions

---
+++ 2022/src/day3.rs
@@ -0,0 +1,79 @@
+fn get_priority(c: char) -> usize {
+    let offset = if c.is_lowercase() { 96 } else { 38 };
+    (c as u8 - offset) as usize
+}
+fn part_one(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| line.trim().split_at(line.len() / 2))
+        .map(|(first, second)| {
+            let mut filtered = first
+                .chars()
+                .filter(|c| second.chars().any(|c2| *c == c2))
+                .collect::<Vec<char>>();
+            filtered.dedup();
+            filtered.into_iter().map(get_priority).sum::<usize>()
+        })
+        .sum()
+}
+
+fn part_two(input: &str) -> usize {
+    let lines = input.lines().collect::<Vec<&str>>();
+
+    lines
+        .chunks(3)
+        .map(|group| {
+            let rucksacks = group
+                .iter()
+                .map(|items| {
+                    items
+                        .chars()
+                        .map(get_priority)
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<Vec<usize>>>();
+            let mut p = 0;
+            for prio in 1..=52 {
+                if rucksacks.iter().all(|r| r.contains(&prio)) {
+                    p = prio;
+                    break;
+                }
+            }
+            p
+        })
+        .sum()
+}
+
+pub fn run() {
+    let input = include_str!("../input/day3/input.txt");
+
+    println!("DAY 2:");
+    println!("Part 1:");
+    println!("{}", part_one(input));
+
+    println!("Part 2:");
+    println!("{}", part_two(input));
+    println!();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"#;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(157, part_one(INPUT));
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(70, part_two(INPUT));
+    }
+}
