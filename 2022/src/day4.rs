ab45cc2fb3aae4a9d8c6409f91efb84d2f9a5113
koopa1338 <sinner1991@gmail.com>
2022-12-04 12:12:50 +0100

    day4 solutions

---
+++ 2022/src/day4.rs
@@ -0,0 +1,83 @@
+fn part_one(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (first, second) = line.split_once(',').unwrap();
+            let ((first_low, first_high), (second_low, second_high)) = (
+                first.split_once('-').unwrap(),
+                second.split_once('-').unwrap(),
+            );
+
+            (
+                first_low.parse::<u8>().unwrap(),
+                first_high.parse::<u8>().unwrap(),
+                second_low.parse::<u8>().unwrap(),
+                second_high.parse::<u8>().unwrap(),
+            )
+        })
+        .filter(|(first_low, first_high, second_low, second_high)| {
+            (first_low >= second_low && first_high <= second_high)
+                || (first_low <= second_low && first_high >= second_high)
+        })
+        .count()
+}
+
+fn part_two(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (first, second) = line.split_once(',').unwrap();
+            let ((first_low, first_high), (second_low, second_high)) = (
+                first.split_once('-').unwrap(),
+                second.split_once('-').unwrap(),
+            );
+
+            (
+                first_low.parse::<u8>().unwrap(),
+                first_high.parse::<u8>().unwrap(),
+                second_low.parse::<u8>().unwrap(),
+                second_high.parse::<u8>().unwrap(),
+            )
+        })
+        .filter(|(first_low, first_high, second_low, second_high)| {
+            (first_low >= second_low && first_low <= second_high)
+                || (first_high >= second_low && first_high <= second_high)
+                || (first_low <= second_low && first_high >= second_low)
+                || (first_low <= second_high && first_high >= second_high)
+        })
+        .count()
+}
+
+pub fn run() {
+    let input = include_str!("../input/day4/input.txt");
+
+    println!("DAY 4:");
+    println!("Part 1:");
+    println!("{}", part_one(input));
+
+    println!("Part 2:");
+    println!("{}", part_two(input));
+    println!();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = r#"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"#;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(2, part_one(INPUT));
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(4, part_two(INPUT));
+    }
+}
